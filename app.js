0062cb4d1e1433f91986a197e1fda2b4cbf528c5
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-04-19 11:57:48 +0530

    created index page

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
 const express = require("express");
 const app = express();
+const {
+  User,
+  Course,
+  Chapter,
+  Pages,
+  Enrollment,
+  CompletedPages,
+} = require("./models");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (request, response) => {
-  response.json("hello");
+  response.render("index", {
+    title: "Learning Management System",
+  });
 });
 
 module.exports = app;
