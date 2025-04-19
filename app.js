4794f3d53a49d60f1cddf82acafb0d477dca43bb
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-04-19 13:59:46 +0530

    Added csrf token and password hashing to signup

--- app.js
+++ app.js
@@ -13,12 +13,19 @@ const {
 } = require("./models");
 const path = require("path");
 const bodyParser = require("body-parser");
+const csrf = require("tiny-csrf");
+const cookieParser = require("cookie-parser");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("shh! some secret string"));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
 
 app.get("/", (request, response) => {
   response.render("index", {
@@ -30,17 +37,18 @@ app.get("/signup/:role", (request, response) => {
   response.render("signup", {
     title: "Sign up",
     role: request.params.role,
+    csrfToken: request.csrfToken(),
   });
 });
 
 app.post("/users", async (request, response) => {
-  console.log(request.body);
+  const hashedPwd = await bcrypt.hash(request.body.password, saltRounds);
   try {
     const user = await User.addUser({
       firstName: request.body.firstName,
       lastName: request.body.lastName,
       email: request.body.email,
-      password: request.body.password,
+      password: hashedPwd,
       role: request.body.role,
     });
     return response.json(user);
